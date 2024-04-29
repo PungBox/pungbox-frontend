6a842e1415e948dbb6dd2e62f62ee0ccf24d4a3a
Yechan Lee <yechan24680@gmail.com>
2024-04-29 21:49:06 +0900

    add .prettierrc.json

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -8,6 +8,5 @@ export default [
   {languageOptions: {globals: globals.browser}},
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
-  pluginReactConfig,
-  {rules: {"react/react-in-jsx-scope": "off"}}
+  pluginReactConfig
 ];
