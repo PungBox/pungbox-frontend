ada6474545bfb2a0de23fd2a6ab30e3231bf0ee5
Yechan Lee <yechan24680@gmail.com>
2024-05-07 15:33:14 +0900

    feat: file list 안에 들어갈 내용

--- src/components/Pages/View.tsx
+++ src/components/Pages/View.tsx
@@ -53,33 +53,62 @@ const FileList = () => {
       modified: new Date().toISOString(),
     },
   ];
-
+  
   function getIconByFileExtension(extension: string) {
-    // TODO: 이미지를 반환하도록 변경할 예정. file extension 더 추가
+    // TODO: 이미지를 반환하도록 변경할 예정.
     switch (extension) {
+      case 'doc':
       case 'docx':
       case 'txt':
+      case 'hwp':
+      case 'odt':
+      case 'pages':
         return '문서';
       case 'png':
       case 'jpg':
       case 'jpeg':
+      case 'gif':
+      case 'bmp':
+      case 'svg':
+      case 'heic':
+      case 'psd':
         return '사진';
       case 'zip':
       case 'gz':
+      case '7z':
+      case 'rar':
         return '압축';
       case 'mov':
       case 'mp4':
+      case 'mpg':
       case 'mpeg':
       case 'avi':
+      case 'm4v':
+      case 'flv':
+      case 'wmv':
         return '동영상';
       case 'mp3':
+      case 'wav':
+      case 'ogg':
+      case 'flac':
         return '음악';
+      case 'xls':
+      case 'xlsx':
+      case 'csv':
+      case 'numbers':
+      case 'ods':
+        return '시트';
+      case 'ppt':
+      case 'pptx':
+        return '발표';
+      case 'pdf':
+        return 'PDF';
       default:
         return '파일';
     }
   }
-
-  function getFileSizeWithSuffix(fileSize: number) {
+  
+  function getFileSizeExpression(fileSize: number) {
     if (fileSize < 1e3) {
       return `${fileSize} KB`;
     } else if (fileSize < 1e6) {
@@ -90,21 +119,42 @@ const FileList = () => {
       return `${Math.floor(fileSize / 1e8) / 10} TB`;
     }
   }
-
-  function getUploadedDatetimeString(created: string) {}
-
+  
+  function getUploadedDatetimeExpression(created: string) {
+    const now = new Date();
+    let datetime;
+    try {
+      datetime = new Date(created);
+    } catch (RangeError) {
+      return null;
+    }
+    
+    let dateExpression;
+    if (now.getDate() === datetime.getDate()) {
+      dateExpression = '오늘';
+    } else if (now.getDate() === datetime.getDate() + 1) {
+      dateExpression = '어제';
+    } else {
+      dateExpression = `${datetime.getMonth() + 1}월 ${datetime.getDate()}일`;
+    }
+    
+    const timeExpression = `${datetime.toLocaleTimeString()}`;
+    
+    return `${dateExpression} ${timeExpression}`;
+  }
+  
   return fileDescriptions.map((file) => {
     const splitted = file.fileName.split('.');
     const extension = splitted[splitted.length - 1];
     const icon = getIconByFileExtension(extension);
-    const fileSize = getFileSizeWithSuffix(file.fileSize);
-    const uploadedDatetime = getUploadedDatetimeString(file.created);
+    const fileSize = getFileSizeExpression(file.fileSize);
+    const uploadedDatetime = getUploadedDatetimeExpression(file.created);
     return (
       <tr>
         <td>{icon}</td>
         <td>{file.fileName}</td>
         <td>{fileSize}</td>
-        <td>{file.created}</td>
+        <td>{uploadedDatetime}</td>
       </tr>
     );
   });
@@ -113,7 +163,7 @@ const FileList = () => {
 const View = () => {
   const storageNumber = 192837;
   const expirationDate = `${new Date().toLocaleDateString()} ${new Date().toLocaleTimeString()}`;
-
+  
   return (
     <div className="view-panel">
       <div className="view-panel-header">
@@ -122,16 +172,16 @@ const View = () => {
       </div>
       <table className="file-list-table">
         <thead>
-          <tr>
-            <th></th>
-            <th>name</th>
-            <th>size</th>
-            <th>upload date</th>
-            <th></th>
-          </tr>
+        <tr>
+          <th></th>
+          <th>name</th>
+          <th>size</th>
+          <th>upload date</th>
+          <th></th>
+        </tr>
         </thead>
         <tbody>
-          <FileList />
+        <FileList />
         </tbody>
       </table>
     </div>
