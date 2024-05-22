a3c79374210f81f11e6edd6a5ea80d8256123941
Jiwon Joung <jiwon@chartmetric.com>
2024-05-22 23:26:29 +0900

    feat: connect UI to view API using dummy bucketId

--- src/components/Pages/view/View.tsx
+++ src/components/Pages/view/View.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect } from 'react';
-import { fetchFileDescriptions } from '../../../utils/dummyData';
 import { FileListTableHeader } from './component/FileListTableHeader';
 import { FileListTableBody } from './component/FileListTableBody';
 import { fileListConfig } from '../../../utils/config';
@@ -8,6 +7,9 @@ import { useFileDescription } from './util/view/fileDescription';
 import styles from '/src/components/Module/View.module.css';
 import { useSelected } from './util/view/selected';
 import { downloadFiles } from './util/view/view';
+import { getFilesFromBucket } from 'service/service';
+
+const DUMMY_BUCKET_ID = '001bc76f-436f-4a7e-a1a0-e1ed389e9262';
 
 const View = () => {
   const storageNumber = 192837;
@@ -22,23 +24,20 @@ const View = () => {
     addFile,
     deleteFiles,
   } = useFileDescription();
-  const {
-    selected, getSelectedFileIds, getSelectedFileUrls, toggleSelectFile,
-  } = useSelected(fileDescriptions);
-  
+  const { selected, getSelectedFileIds, getSelectedFileUrls, toggleSelectFile } = useSelected(fileDescriptions);
+
   useEffect(() => {
-    fetchFileDescriptions().then((fileDescriptions) => {
+    //@TODO: replace DUMMY_BUCKET_ID with actual bucketId
+    getFilesFromBucket({ bucketId: DUMMY_BUCKET_ID }).then((fileDescriptions) => {
       displayFileDescriptions(fileDescriptions);
       resetToDefaultSortingOrder(fileListConfig.defaultSortingCriteria);
     });
   }, []);
-  
+
   useEffect(() => {
     reSortFileDescriptions(fileDescriptions, setFileDescriptions);
   }, [reSortFileDescriptions]);
-  
-  
-  // TODO: dummy json 사용 중이지만, backend로부터 가져오도록 변경해야 함
+
   // TODO 추가: 인증키 유효하지 않으면 Expired 페이지로 이동하게 (만료일자 및 고유번호 포함)
   return (
     <div className={styles.view_panel}>
@@ -56,29 +55,29 @@ const View = () => {
       </div>
       <table className={styles.file_list_table}>
         <thead>
-        <FileListTableHeader
-          handleSorting={handleSorting}
-          sortingCriteria={sortingCriteria}
-          isSortingAscending={isSortingAscending}
-        />
+          <FileListTableHeader
+            handleSorting={handleSorting}
+            sortingCriteria={sortingCriteria}
+            isSortingAscending={isSortingAscending}
+          />
         </thead>
         <tbody>
-        <FileListTableBody
-          fileDescriptions={fileDescriptions}
-          isFileDescriptionsLoaded={isFileDescriptionsLoaded}
-          selected={selected}
-          toggleSelectFile={toggleSelectFile}
-        />
+          <FileListTableBody
+            fileDescriptions={fileDescriptions}
+            isFileDescriptionsLoaded={isFileDescriptionsLoaded}
+            selected={selected}
+            toggleSelectFile={toggleSelectFile}
+          />
         </tbody>
         <tfoot>
-        <tr>
-          <td colSpan={6}>
-            <label htmlFor="file_upload" className={styles.file_upload_label}>
-              <input type="file" id="file_upload" className={styles.file_upload_input} onChange={addFile} />
+          <tr>
+            <td colSpan={6}>
+              <label htmlFor="file_upload" className={styles.file_upload_label}>
+                <input type="file" id="file_upload" className={styles.file_upload_input} onChange={addFile} />
                 Upload File
-            </label>
-          </td>
-        </tr>
+              </label>
+            </td>
+          </tr>
         </tfoot>
       </table>
     </div>
--- src/service/service.ts
+++ src/service/service.ts
@@ -1,6 +1,33 @@
-export const getPresignedUrl = async ({files, bucketName, threshold = 1024}: {files: File[]; bucketName: string; threshold?: number }) => {
+//@TODO: endpoint 는 env 환경변수로 관리하도록 수정
+const DEFAULT_ENDPOINT = 'https://nmxnc3h7a8.execute-api.ap-northeast-2.amazonaws.com/production';
+
+const generateEndpoint = ({
+  endpoint,
+  params = {},
+}: {
+  endpoint: string;
+  params?: Record<string, string | number>;
+}) => {
+  return `${DEFAULT_ENDPOINT}${endpoint}${
+    params
+      ? `?${Object.keys(params)
+          .map((key) => `${key}=${params[key]}`)
+          .join('&')}`
+      : ''
+  }`;
+};
+
+export const getPresignedUrl = async ({
+  files,
+  bucketName,
+  threshold = 1024,
+}: {
+  files: File[];
+  bucketName: string;
+  threshold?: number;
+}) => {
   const response = await fetch(
-    `https://fhxljaiclxngspozkr55elwaba0ifvek.lambda-url.us-west-2.on.aws/?bucketName=${bucketName}&threshold=${threshold}`,
+    generateEndpoint({ endpoint: '/bucket/presigned-url', params: { bucketName, threshold } }),
     {
       method: 'POST',
       headers: {
@@ -8,8 +35,19 @@ export const getPresignedUrl = async ({files, bucketName, threshold = 1024}: {fi
       },
 
       body: JSON.stringify({ files }),
-    }
+    },
   );
   const data = await response.json();
   return data.url;
-}
+};
+
+export const getFilesFromBucket = async ({ bucketId }: { bucketId: string }) => {
+  const response = await fetch(generateEndpoint({ endpoint: '/bucket/files', params: { bucketId } }), {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+  const data = await response.json();
+  return data.files;
+};
