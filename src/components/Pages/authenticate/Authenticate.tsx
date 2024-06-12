1157fce71919cde9f0416813b2816895b8520690
Yechan Lee <yechan24680@gmail.com>
2024-06-12 22:03:08 +0900

    feat: /authenticate page - submit()

--- src/components/Pages/authenticate/Authenticate.tsx
+++ src/components/Pages/authenticate/Authenticate.tsx
@@ -1,20 +1,59 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useAccessCodeInput } from './util/accessCodeInput';
 import { authConfig as config } from '../../../utils/config';
 import styles from '/src/components/Module/Authenticate.module.css';
+import { authenticate } from '../../../service/service';
+import { HTMLFormElement, IHTMLFormControlsCollection } from 'happy-dom';
+import { useNavigate } from 'react-router-dom';
+
+interface RegisterFormElements extends IHTMLFormControlsCollection {
+  password: HTMLInputElement;
+  digit0: HTMLInputElement;
+  digit1: HTMLInputElement;
+  digit2: HTMLInputElement;
+  digit3: HTMLInputElement;
+  digit4: HTMLInputElement;
+  digit5: HTMLInputElement;
+  digit6?: HTMLInputElement;
+  digit7?: HTMLInputElement;
+  digit8?: HTMLInputElement;
+  digit9?: HTMLInputElement;
+}
+
+interface RegisterFormElement extends HTMLFormElement {
+  readonly elements: RegisterFormElements;
+}
+
+function getBucketIdFromFormElem(formElements: RegisterFormElements): string {
+  let result = '';
+  for (let i = 0; i < 10; i++) {
+    const elementName = `digit${i}` as `digit${0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9}`;
+    const digitElem = formElements[elementName];
+    if (digitElem === undefined) break;
+    result += digitElem.value;
+  }
+  return result;
+}
 
 const Authenticate = () => {
   const { inputsRef, undoDigitInput, accessCodeInput } = useAccessCodeInput();
-
-  function submit(e: React.FormEvent<HTMLFormElement>) {
-    console.log('submit');
-    console.log(e);
+  const [isLoading, setIsLoading] = useState(false);
+  const navigate = useNavigate();
+  
+  async function submit(e: React.FormEvent) {
+    e.preventDefault();
+    setIsLoading(true);
+    const formElements = (e.currentTarget as unknown as RegisterFormElement).elements;
+    const bucketId = getBucketIdFromFormElem(formElements);
+    await authenticate({ bucketId, password: formElements.password.value });
+    setIsLoading(false);
+    navigate('/view');
   }
   
   return (
     <div className={styles.authPanel}>
-      <form method="POST" onSubmit={(e) => submit(e)}>
-      <span className="font-bold">Access Code:</span>
+      <form method="POST" onSubmit={async (e) => await submit(e)}>
+        <span className="font-bold">Access Code:</span>
         <label htmlFor="access_code" className={styles.label}>
           {accessCodeInput}
           <span className={`${styles.materialSymbolsOutlined} ${styles.cancel}`} onClick={undoDigitInput}>
@@ -24,22 +63,22 @@ const Authenticate = () => {
         
         <span className="font-bold">Password:</span>
         <label htmlFor="password" className={styles.label}>
-
-        <input
-          ref={(elem) => (inputsRef.current[config.DIGIT_LENGTH] = elem)}
-          type="password"
-          id="password"
-          name="password"
-          required
-          className={styles.inputpassword}
-        />
-      </label>
-  
-      <button type="submit" value="Submit" className={styles.button}>
-        Go to Storage
-      </button>
-    </form>
-  </div>
+          
+          <input
+            ref={(elem) => (inputsRef.current[config.DIGIT_LENGTH] = elem)}
+            type="password"
+            id="password"
+            name="password"
+            required
+            className={styles.inputpassword}
+          />
+        </label>
+        
+        <button type="submit" value="Submit" className={styles.button}>
+          Go to Storage
+        </button>
+      </form>
+    </div>
   );
 };
 
