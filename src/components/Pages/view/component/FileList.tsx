5e7935995ac9e26b88677a181f559b011aeee861
Jiwon Joung <jiwon@chartmetric.com>
2024-06-15 18:27:11 +0900

    feat: display only undeleted files & only merged files are available

--- src/components/Pages/view/component/FileList.tsx
+++ src/components/Pages/view/component/FileList.tsx
@@ -13,15 +13,29 @@ export const FileList = ({ fileDescriptions = [], selected, toggleSelectFile }: 
     const splitted = file.fileName.split('.');
     const extension = splitted[splitted.length - 1];
     return (
-      <tr className={styles.checkbox} key={file.id} onClick={() => toggleSelectFile(file.id)}>
-        <td>
-          <input type="checkbox" checked={selected[file.id]} onChange={() => toggleSelectFile(file.id)} />
-        </td>
-        <td>{getIconByFileExtension(extension)}</td>
-        <td>{getFileNameExpression(file.fileName)}</td>
-        <td>{getFileSizeExpression(file.fileSize)}</td>
-        <td>{getUploadedDatetimeExpression(file.createdAt)}</td>
-      </tr>
+      !file.deleted && (
+        <tr className={styles.checkbox} key={file.id} onClick={() => toggleSelectFile(file.id)}>
+          <td>
+            <input
+              type="checkbox"
+              checked={selected[file.id]}
+              disabled={!file.merged}
+              onChange={() => toggleSelectFile(file.id)}
+            />
+          </td>
+          <td>{getIconByFileExtension(extension)}</td>
+          <td>{getFileNameExpression(file.fileName)}</td>
+          <td>{getFileSizeExpression(file.fileSize)}</td>
+          <td>{getUploadedDatetimeExpression(file.createdAt)}</td>
+          <td>
+            {file.merged ? (
+              <span style={{ backgroundColor: 'greenYellow', color: 'white' }}>Available</span>
+            ) : (
+              <span style={{ backgroundColor: 'tomato', color: 'white' }}>'Unavailable'</span>
+            )}
+          </td>
+        </tr>
+      )
     );
   });
 };
